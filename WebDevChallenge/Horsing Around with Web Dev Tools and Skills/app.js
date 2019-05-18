6edcd8aa9fef435d8e61cd3e8502ffaab416c313
Rajasekar M <36572895+rajasekar1999@users.noreply.github.com>
2019-05-18 23:01:56 +0530

    Update app.js

--- WebDevChallenge/Horsing Around with Web Dev Tools and Skills/app.js
+++ WebDevChallenge/Horsing Around with Web Dev Tools and Skills/app.js
@@ -1,3 +1,10 @@
-function numsel() {
-    document.getElementById("display").innerHTML = "1";
-}
+$(document).ready(function(){
+    $('div[id^="number"]').click(function() {
+        var num = $(this).attr('value');     
+        $('#display').html(num);
+        $('#number'+num).css("background-color", "grey");
+        setTimeout(function(){
+            $('#number'+num).css("background-color", "lemonchiffon");
+        }, 100);
+    });
+  });
