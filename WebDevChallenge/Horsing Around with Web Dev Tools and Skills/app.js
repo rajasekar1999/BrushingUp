a33c57b1c2a2d3183c4d0e4807cc06e19fb14b38
Rajasekar M <36572895+rajasekar1999@users.noreply.github.com>
2019-05-20 22:53:01 +0530

    Update app.js

--- WebDevChallenge/Horsing Around with Web Dev Tools and Skills/app.js
+++ WebDevChallenge/Horsing Around with Web Dev Tools and Skills/app.js
@@ -1,10 +1,38 @@
-$(document).ready(function(){
-    $('div[id^="number"]').click(function() {
+$(document).ready(function()
+{
+    $('div[id^="number"]').click(function() 
+    {
         var num = $(this).attr('value');     
         $('#display').html(num);
         $('#number'+num).css("background-color", "grey");
-        setTimeout(function(){
+        setTimeout(function()
+        {
             $('#number'+num).css("background-color", "lemonchiffon");
         }, 100);
     });
-  });
+
+    $('#delete').click(function() 
+    {    
+        $('#display').html("");
+        $('#delete').css("background-color", "aqua");
+        setTimeout(function()
+        {
+            $('#delete').css("background-color", "lemonchiffon");
+        }, 100);    
+    });
+
+    $('div[id^="operator"]').click(function() 
+    {
+        var op = $(this).attr('value');
+        $('#operator'+op).css("background-color", "orange");
+    
+        $('#operator5').click(function() 
+        {
+            setTimeout(function()
+            {
+                $('#operator'+op).css("background-color", "lemonchiffon");
+                $('#operator5').css("background-color", "lemonchiffon");
+            }, 100);
+        });
+    });
+});
